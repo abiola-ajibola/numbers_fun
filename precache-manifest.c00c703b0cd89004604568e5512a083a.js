56c71d47bafec664397fdcea0d699657c650ae00
Abiola <ajibolaabiolaahmed@gmail.com>
2020-05-05 05:40:33 +0100

    Updates

---
+++ precache-manifest.c00c703b0cd89004604568e5512a083a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8e3b1a6f7b114e44eca93733950998",
+    "url": "/numbers_fun/index.html"
+  },
+  {
+    "revision": "077587562866ce76893f",
+    "url": "/numbers_fun/static/css/main.55eea6c9.chunk.css"
+  },
+  {
+    "revision": "71cbfb81837b0ec0911c",
+    "url": "/numbers_fun/static/js/2.e059f2dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numbers_fun/static/js/2.e059f2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077587562866ce76893f",
+    "url": "/numbers_fun/static/js/main.33a4eeaf.chunk.js"
+  },
+  {
+    "revision": "7e334ffc9253f36b83b9",
+    "url": "/numbers_fun/static/js/runtime-main.25618772.js"
+  }
+]);
