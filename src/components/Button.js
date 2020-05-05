7bb4c22f9ad866e83b3906d2b4d04d6d74a76a06
Abiola <ajibolaabiolaahmed@gmail.com>
2020-05-05 05:24:22 +0100

    clean slate

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Button = ({clicked, text}) => {
-    return(
-    <button className ='button' onClick ={clicked}>{text}</button>
-    )
-}
--- src/components/DateInput.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const DateInput = ({changedDate}) => {
-    return(
-    <input className='input' type='date' placeholder='Pick a date' onChange={changedDate}></input>
-    )
-}
